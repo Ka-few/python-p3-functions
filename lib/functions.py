dc5f81272f13160cc67f8d25963b60dc88dab8f0
ka-few <njorovista@gmail.com>
2025-08-04 14:21:45 +0300

    python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    greet = "Hello, programmer!"
+    print(greet)
+    
+greet_programmer()
+    
+  
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
+greet("Guido")
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
+greet_with_default()
+greet_with_default("Guido")
+    
+    
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+        
+add(45, 55)
 
 def halve(number):
-    pass
+    return number/2    
+    
+halve(99)
